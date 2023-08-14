3799fff59ca857f787377ab771843cf725e8e075
Manuel <bymanudominguez@gmail.com>
2023-08-14 17:15:08 -0300

    [ADD] Loan & LoanRepository

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,21 +1,17 @@
 package com.mindhub.homebanking;
 
-import com.mindhub.homebanking.dtos.AccountDTO;
-import com.mindhub.homebanking.models.Account;
-import com.mindhub.homebanking.models.Client;
-import com.mindhub.homebanking.models.Transaction;
-import com.mindhub.homebanking.models.TransactionType;
+import com.mindhub.homebanking.models.*;
 import com.mindhub.homebanking.repositories.AccountRepository;
 import com.mindhub.homebanking.repositories.ClientRepository;
+import com.mindhub.homebanking.repositories.LoanRepository;
 import com.mindhub.homebanking.repositories.TransactionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import javax.persistence.Id;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -25,13 +21,29 @@ public class HomebankingApplication {
 	}
 
 	@Bean
-	public CommandLineRunner init (ClientRepository clientRepository, AccountRepository accountRepository, TransactionRepository transactionRepository){
+	public CommandLineRunner init (ClientRepository clientRepository, AccountRepository accountRepository, TransactionRepository transactionRepository, LoanRepository loanRepository){
 		return args -> {
 			Client client1 = new Client("Melba", "Morel", "melba@mindhub.com");
 			clientRepository.save(client1);
 
 			Client client2 = new Client("Lucia", "Colombo", "lucolombo@gmail.com");
 			clientRepository.save(client2);
+
+			Loan loan1 = new Loan("Hipotecario");
+			loan1.setMaxAmount(500.000);
+			loan1.setPayments(List.of(12,24,36,48,60));
+
+			Loan loan2 = new Loan("Personal");
+			loan2.setMaxAmount(100.000);
+			loan2.setPayments(List.of(6, 12, 24));
+
+			Loan loan3 = new Loan("Automotriz");
+			loan3.setMaxAmount(300.000);
+			loan3.setPayments(List.of(6,12,24,36));
+
+			loanRepository.save(loan1);
+			loanRepository.save(loan2);
+			loanRepository.save(loan3);
 
 			Account account1 = new Account();
 			account1.setNumber("VIN001");
---
+++ src/main/java/com/mindhub/homebanking/models/Loan.java
@@ -0,0 +1,55 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+    private String name;
+    private Double maxAmount;
+    @ElementCollection
+    private List<Integer> payments = new ArrayList<>();
+
+    public Loan(){
+
+    }
+
+    public Loan(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getMaxAmount() {
+        return maxAmount;
+    }
+
+    public void setMaxAmount(Double maxAmount) {
+        this.maxAmount = maxAmount;
+    }
+
+    public List<Integer> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(List<Integer> payments) {
+        this.payments = payments;
+    }
+}
