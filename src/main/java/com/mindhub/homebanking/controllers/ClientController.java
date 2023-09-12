3ddf4cc01522a00048b86173b94dfb5bdc135c19
Manuel <bymanudominguez@gmail.com>
2023-09-12 20:58:45 -0300

    Integration test

--- src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -43,7 +43,7 @@ public class ClientController {
         return clientService.getClients();
     }
 
-    @RequestMapping(path = "/clients", method = RequestMethod.POST)
+    @PostMapping("/clients")
     public ResponseEntity<Object> register(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String password) {
 
         if (firstName.isBlank()) {
