81fc61e16703791e744876bcd84099ab38fa58a0
Manuel <bymanudominguez@gmail.com>
2023-09-19 17:25:17 -0300

    [UPDATE]

---
+++ src/test/java/com/mindhub/homebanking/controllers/ClientControllerTest.java
@@ -0,0 +1,35 @@
+package com.mindhub.homebanking.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ClientControllerTest {
+
+    @Test
+    void getClients() throws Exception {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void getClient() {
+    }
+
+    @Test
+    void connection() {
+    }
+
+    @Test
+    void getCurrent() {
+    }
+
+    @Test
+    void getRandomNumber() {
+    }
+}
